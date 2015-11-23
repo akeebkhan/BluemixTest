bb9561b92d29dd02e88f0929dad1d0c9c2fa6252
unknown <akeeb.khan@MW7CBCESHTB6MA.dir.svc.accenture.com>
2015-11-23 15:14:47 +0000

    JavaScript HelloWorld File

---
+++ src/Hello.js
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+<head> <title> Welcome to this world! </title> </head>
+
+<body>
+<p> header.... </p>
+
+<script>
+
+  alert("Hello World!");
+</script>
+
+<p> Footer... </p>
+
+</body>
+</html>
